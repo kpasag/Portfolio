006c71ea6cbbccbbe82fb7e88011a005474f614b
Kyle Pasag <kylepasag@gmail.com>
2026-03-23 03:38:31 -0700

    Update href

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import Navbar from "./Navbar.jsx";
 import Body from "./Body.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
+  useEffect(() => {
+    const anchors = document.querySelectorAll('a[href^="#"]');
+    const handler = function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) target.scrollIntoView({ behavior: "smooth", block: "start" });
+    };
+    anchors.forEach((a) => a.addEventListener("click", handler));
+    return () => anchors.forEach((a) => a.removeEventListener("click", handler));
+  }, []);
+
   return (
     <>
       <Navbar />
